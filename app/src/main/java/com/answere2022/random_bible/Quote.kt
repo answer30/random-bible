497b205d9b2b578e73d8a074b98b23d07417de63
answere3030 <answere3030@gmail.com>
2022-09-12 17:24:29 +0900

    1st

---
+++ app/src/main/java/com/answere2022/random_bible/Quote.kt
@@ -0,0 +1,6 @@
+package com.answere2022.random_bible
+
+data class Quote(
+    val quote : String,
+    val name : String
+)
---
+++ app/src/main/java/com/answere2022/random_bible/QuotePagerAdapter.kt
@@ -0,0 +1,42 @@
+package com.answere2022.random_bible
+
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class QuotePagerAdapter(
+    private val quotes: List<Quote>
+) : RecyclerView.Adapter<QuotePagerAdapter.QuoteViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        QuoteViewHolder(
+
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_quote, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
+        holder.bind(quotes[position])
+    }
+
+    override fun getItemCount() = quotes.size
+
+    class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+        private val quoteTextView: TextView = itemView.findViewById(R.id.quoteTextView)
+        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+
+
+        fun bind(quote: Quote) {
+            quoteTextView.text = quote.quote
+            nameTextView.text = quote.name
+
+        }
+
+    }
+}
